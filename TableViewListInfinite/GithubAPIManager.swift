82a565246a965fa8d369a2f1ddbad28e1489e2a1
Mohd Hafiz <xmhafiz@gmail.com>
2021-06-23 22:04:09 +0800

    Added API service and User model

--- TableViewListInfinite/GithubAPIManager.swift
+++ TableViewListInfinite/GithubAPIManager.swift
@@ -1,1 +1,56 @@
+//
+//  GithubAPIManager.swift
 
+import Foundation
+// 1
+enum APIError: Error {
+    case invalidUrl
+    case errorDecode
+    case failed(error: Error)
+    case unknownError
+}
+
+struct GithubAPIManager {
+    static let shared = GithubAPIManager()
+    
+    /// Get all users from https://docs.github.com/en/rest/reference/users#list-users
+    /// - Parameters:
+    ///   - perPage: Results per page (max 100). Default: 30
+    ///   - sinceId: A user ID. Only return users with an ID greater than this ID.
+    func getUsers(perPage: Int = 30, sinceId: Int? = nil, completion: @escaping (Result<[User], APIError>) -> Void) {
+        // 4
+        var components = URLComponents(string: "https://api.github.com/users")!
+        components.queryItems = [
+            URLQueryItem(name: "per_page", value: "\(perPage)"),
+            URLQueryItem(name: "since", value: (sinceId != nil) ? "\(sinceId!)" : "")
+        ]
+        guard let url = components.url else {
+            completion(.failure(.invalidUrl))
+            return
+        }
+        // 3
+        let urlRequest = URLRequest(url: url, timeoutInterval: 10)
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error  in
+            if error != nil {
+                print("dataTask error: \(error!.localizedDescription)")
+                completion(.failure(.failed(error: error!)))
+            } else if let data = data {
+                // Success request
+                do {
+                    // 4. Decode json into array of User
+                    let users = try JSONDecoder().decode([User].self, from: data)
+                    print("success")
+                    completion(.success(users))
+                } catch {
+                    // Send error when decoding
+                    print("decoding error")
+                    completion(.failure(.errorDecode))
+                }
+            } else {
+                print("unknown dataTask error")
+                completion(.failure(.unknownError))
+            }
+        }
+        .resume()
+    }
+}
--- TableViewListInfinite/User.swift
+++ TableViewListInfinite/User.swift
@@ -1,1 +1,47 @@
+//
+//  User.swift
+//  TableViewListInfinite
+//
+//  Created by Hafiz on 23/06/2021.
+//
+
+import Foundation
+
+struct User: Decodable {
+    let id: Int
+    let name: String
+    let avatarUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name = "login"
+        case avatarUrl = "avatar_url"
+    }
+}
+
+/*
+ Sample User JSON object
+ 
+ {
+ "login": "mojombo",
+ "id": 1,
+ "node_id": "MDQ6VXNlcjE=",
+ "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+ "gravatar_id": "",
+ "url": "https://api.github.com/users/mojombo",
+ "html_url": "https://github.com/mojombo",
+ "followers_url": "https://api.github.com/users/mojombo/followers",
+ "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+ "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+ "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+ "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+ "organizations_url": "https://api.github.com/users/mojombo/orgs",
+ "repos_url": "https://api.github.com/users/mojombo/repos",
+ "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+ "received_events_url": "https://api.github.com/users/mojombo/received_events",
+ "type": "User",
+ "site_admin": false
+ }
+ 
+ */
 
